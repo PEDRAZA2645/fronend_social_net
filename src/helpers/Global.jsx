049ffead2322fa12480b8692ac06dfb865037016
JEFFERSON ANDRES MORENO PEDRAZA <jamp9504@gmail.com>
2024-11-05 19:04:23 -0500

    finish frontend

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,10 @@
+// Ruta del Backend desplegado
+ export const Global = {
+   url: "https://backend-social-net-ajus.onrender.com/api/"
+ }
+
+
+// Ruta del Backend en Local: se debe ejecutar el servidor del backend
+  // export const Global = {
+  //   url: "http://localhost:3900/api/"
+  // }
